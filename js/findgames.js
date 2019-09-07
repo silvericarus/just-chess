50b8b1ed89fe985405a454ead448e7ea1d54bb6f
Silver Icarus <albin2@yahoo.com>
2019-09-07 12:44:46 +0200

    Añadida función al listado de partidas disponibles

--- js/findgames.js
+++ js/findgames.js
@@ -1,3 +1,31 @@
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+function startGame(id){
+    window.location = "./index.html";
+    setCookie("gameId",id,1);
+}
+
 function findGames(){
     var gamestable = document.getElementById("gamestable");
     gamestable.innerHTML = "";
@@ -12,9 +40,16 @@ function findGames(){
             var row = document.createElement("tr");
             var cellId = document.createElement("td");
             var cell = document.createElement("td");
-            cellId.textContent = doc.id;
+            var cellBtn = document.createElement("td");
+            var btn = document.createElement("button");
+            btn.classList.add("button","primary");
+            btn.addEventListener("click",function(){
+                startGame(doc.id);
+            });
+            cellId.textContent = data["name"];
             cell.textContent = data["state"];
-            row.append(cellId,cell);
+            cellBtn.appendChild(btn);
+            row.append(cellId,cell,cellBtn);
             gamestable.append(row);
         });
     })
